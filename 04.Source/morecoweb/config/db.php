8cbd39a99752aa28a2926e427266d71acc7529ed
umbalaconmeogia <umbalaconmeogia@gmail.com>
2016-07-02 10:51:31 +0900

    Add DB design

--- 04.Source/morecoweb/config/db.php
+++ 04.Source/morecoweb/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=morecodb;port=5432;dbname=moreco', 
+    'username' => 'moreco',
+    'password' => 'QZoNYD2nbp',
     'charset' => 'utf8',
 ];
